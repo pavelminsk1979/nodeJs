ff07a5ec3c0894cb94391e41b39ac3049344adb0
pavelminsk1979 <pavelminsk1979@mail.ru>
2023-04-10 20:12:27 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const nodemailer = require("nodemailer");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+
+const app = express()
+app.use(cors())
+const port = 3010
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use(bodyParser.json())
+
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'pavvel.potapov@gmail.com',
+        pass: 'qefajnoxhxnbqnul',
+    },
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.post('/message', async function (req, res) {
+
+    let {message, email, name} = req.body
+    let info = await transporter.sendMail({
+        from: 'MY PORTFOLIO',
+        to: "pavelminsk1979@mail.ru",
+        subject: "portfolio",
+        html: `<b>Сообщение из моего портфолио</b>
+<div>От кого:${name}</div>
+<div>Контакт:${email}</div>
+<div>Текст сообщения :${message}</div>`,
+    });
+
+    res.send(req.body)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
